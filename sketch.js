40281b2a82373f23252c97c65fca84ae9122e6df
Shuangu <wshuang661@gmail.com>
2024-09-22 22:41:26 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
 }
+background(219,209,207);
+
+rect(50,100,30,90);
+line(90,150,38,69);
+
+
+
 
 function draw() {
 }
